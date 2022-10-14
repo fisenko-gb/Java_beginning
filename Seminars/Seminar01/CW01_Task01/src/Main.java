85ff2a13304dcfc48f254e3fd52a6cf222983fef
fisenko-gb <fisenko-gb@yandex.ru>
2022-10-14 22:59:02 +0400

    Семинар 1

--- Seminars/Seminar01/CW01_Task01/src/Main.java
+++ Seminars/Seminar01/CW01_Task01/src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        byte a = 1;
+        byte b = 7;
+        byte c = 2;
+        byte d = 1;
+        int rez_temp = a;
+
+        String rezult = null;
+
+        while (rez_temp <= b){
+            rez_temp = multiply(rez_temp, c);
+            rezult = rezult + "k1";
+
+            if (rez_temp == b){
+                break;
+            }
+
+            rez_temp = addition(rez_temp, d);
+            rezult = rezult + "k2";
+            if (rez_temp == b){
+                break;
+            }
+        }
+
+
+
+        System.out.println(rezult);
+
+    }
+
+    public static int multiply(int a, int c){
+        return a * c;
+    }
+
+    public static int addition(int a, int c){
+        return a + c;
     }
 }
