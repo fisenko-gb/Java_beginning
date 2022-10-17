13ee15235040f6232dcadc73718ce459f5c64198
fisenko-gb <fisenko-gb@yandex.ru>
2022-10-17 21:41:36 +0400

    HW01

--- Seminars/Seminar01/Homework01/HW01_Task01/src/Main.java
+++ Seminars/Seminar01/Homework01/HW01_Task01/src/Main.java
@@ -1,5 +1,24 @@
+// Написать программу вычисления n-ого треугольного числа
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int number = scan.nextInt();
+        scan.close();
+
+        int rez = triangular_numbers(number);
+
+        System.out.println("Число " + rez + ", является " + number + "-м треугольным");
+    }
+
+    public static int triangular_numbers(int n) {
+        if(n==1)
+        return 1;
+    else
+        return( n + triangular_numbers(n-1) );
     }
 }
