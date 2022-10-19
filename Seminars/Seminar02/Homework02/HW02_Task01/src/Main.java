698bd154f338acb91d3965c0f5ea206bbf661e2e
fisenko-gb <fisenko-gb@yandex.ru>
2022-10-19 19:34:45 +0400

    HW2_Task1

--- Seminars/Seminar02/Homework02/HW02_Task01/src/Main.java
+++ Seminars/Seminar02/Homework02/HW02_Task01/src/Main.java
@@ -1,5 +1,15 @@
+// Написать программу, показывающую последовательность действий для игры “Ханойская башня”
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        get_game_algorim('A', 'B', 'C', 3);
+    }
+
+    static void get_game_algorim(char A, char B, char C, int n) {
+        if(n > 0) {
+            get_game_algorim(A, C, B,n-1);
+            System.out.println("Переложить с " + A + " на " + B);
+            get_game_algorim(C, B, A,n-1);
+        }
     }
 }
