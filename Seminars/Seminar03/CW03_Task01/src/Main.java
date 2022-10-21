7ebce32b77ea93dcf28ffcf10adbd42972399046
fisenko-gb <fisenko-gb@yandex.ru>
2022-10-21 23:05:51 +0400

    Seminar03

--- Seminars/Seminar03/CW03_Task01/src/Main.java
+++ Seminars/Seminar03/CW03_Task01/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+
 public class Main {
+
+    static void myadd (ArrayList<Integer> t_array, int item){
+
+        t_array.add(item);
+
+    }
+    static void myremove(ArrayList<Integer> t_array){
+        t_array.remove(0);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<Integer> t_array = new ArrayList<Integer>();
+
+        myadd(t_array, 2);
+        System.out.println(t_array);
+        myadd(t_array, 12);
+        System.out.println(t_array);
+        myadd(t_array,1);
+        System.out.println(t_array);
+
+        myremove(t_array);
+        System.out.println(t_array);
+        myremove(t_array);
+        System.out.println(t_array);
+
+        myadd(t_array, 14);
+        System.out.println(t_array);
+
     }
 }
