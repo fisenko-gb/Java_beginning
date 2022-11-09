5432e35526b272397aa98866ff648aefbe85d59f
fisenko-gb <fisenko-gb@yandex.ru>
2022-11-09 13:55:16 +0400

    HW04 quickSort

---
+++ Seminars/Seminar04/Homework04/HW04_Task01/src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] t_array = {7, 6, 3, 1, 3 , 2, 1};
+        System.out.println("Исходный массив: ");
+        print_array(t_array);
+        System.out.println("Отсортированный массив: ");
+        quickSort(t_array,0, t_array.length - 1);
+        print_array(t_array);
+
+    }
+    public static void print_array(int[] t_array){
+        for (int i = 0; i < t_array.length; i++){
+            System.out.print(t_array[i] + ", " );
+        }
+        System.out.println();
+    }
+    public static void quickSort(int[] t_array, int leftLimit, int rightLimit) {
+        int temp_leftLimit = leftLimit;
+        int temp_rightLimit = rightLimit;
+        int current = t_array[(temp_leftLimit + temp_rightLimit) / 2];
+        do {
+            while (t_array[temp_leftLimit] < current) {
+                temp_leftLimit++;
+            }
+
+            while (t_array[temp_rightLimit] > current) {
+                temp_rightLimit--;
+            }
+
+            if (temp_leftLimit <= temp_rightLimit) {
+
+                if (temp_leftLimit < temp_rightLimit) {
+                    int tmp = t_array[temp_leftLimit];
+                    t_array[temp_leftLimit] = t_array[temp_rightLimit];
+                    t_array[temp_rightLimit] = tmp;
+                }
+
+                temp_leftLimit++;
+                temp_rightLimit--;
+            }
+        } while (temp_leftLimit <= temp_rightLimit);
+
+
+        if (temp_leftLimit < rightLimit) {
+            quickSort(t_array, temp_leftLimit, rightLimit);
+        }
+        if (leftLimit < temp_rightLimit) {
+            quickSort(t_array, leftLimit, temp_rightLimit);
+        }
+    }
+}
